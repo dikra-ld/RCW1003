c1bc510b76d36e70b794e04f05bad368dac3948f
Dikra <lddkr697@gmail.com>
2025-05-30 09:48:26 -0400

    changes
    changes on client.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-#afficher les numeros de 1 a 10
-for i in range(1,11):
-    print(i)
+from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+app=FastAPI()
+@app.get("/")
+async def welcome():
+    try:
+        return {"message":"Hello from RCW1003"}
+    except Exception as e:
+        print (f'Exception: {e}')
+        raise HTTPExceptio(status_code=500 , detail=str(e))
